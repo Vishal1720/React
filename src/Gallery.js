ecc8b12037ec16fb01408239c008a9957927685f
Vishal <vishal198shetty@gmail.com>
2024-02-25 16:37:26 +0530

    added delete functionality in Gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import Form from './Form'
-
+import bin from './bins2.png'
 const Gallery = () => {
 
-    const [gallery,setgallery]=useState([{url:"https://www.imgmacchine.it/wp-content/uploads/2023/07/1000x1000px_IMG_slogan-02-1024x1024.png",desc:'New image'}])
+    const [gallery,setgallery]=useState([{url:"https://media.tenor.com/2vzT-8oPXl8AAAAi/tkthao219-bubududu.gif",desc:'Cat'}
+  ,{url:"https://media.tenor.com/mNYDqm1QN40AAAAi/cool-swag.gif",desc:'Yo'}])
    const [showgal,setshowgal]=useState(true);
     const settheurl=(src,descr)=>{
         setgallery([...gallery,{url:src,desc:descr}])
         console.log(descr)
+    }
+
+    const deleteit=(url,desc)=>{
+      setgallery(gallery.filter((item)=>{return url!==item.url && desc!==item.desc}))
     }
 
 
@@ -25,8 +30,8 @@ const Gallery = () => {
       <div id="Gallery">
         {
         gallery.map((e)=>{
-         return <figure style={{width:'fit-content',height:'fit-content'}}><img key={e.url} style={{objectFit:"cover",margin:'2px'}} height="400px" width='500px' src={e.url} alt="Images"/>
-<figcaption style={{textAlign:'center',fontWeight:'bold',fontSize:'1.5rem'}}>{e.desc}</figcaption>
+         return <figure style={{width:'fit-content',height:'fit-content'}}><img key={e.url} style={{objectFit:"cover",width:'fit-content',margin:'2px'}} height="400px" width='500px' src={e.url} alt="Images"/>
+<figcaption style={{textAlign:'center',fontWeight:'bold',fontSize:'1.5rem'}}>{e.desc}<img onClick={()=>{deleteit(e.url,e.desc)}} style={{width:'3vw',height:'3vw',display:'inline'}}  alt="del icon" src={bin}></img></figcaption>
 </figure>
          
         })
